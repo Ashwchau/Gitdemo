aa8e39db386f95a871640245a5cc37c79c5ca5dd
Ashwchau <162965747+Ashwchau@users.noreply.github.com>
2024-03-14 16:37:59 +0530

    Add files via upload

---
+++ src/main/java/com/Section6/Locator2.java
@@ -0,0 +1,19 @@
+package Section6;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locator2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.getProperty("webdriver.chrome.driver", "C:/Users/Ashwchau/OneDrive - Capgemini/Documents/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		System.out.println(driver.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).getText());
+		System.out.println(driver.findElement(By.xpath("//header/div/button[1]/parent::div/button[2]")).getText());
+
+	}
+
+}
