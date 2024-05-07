b4cafb0bb982ea39332427bb2040a5c192e71bf8
Ashwchau <162965747+Ashwchau@users.noreply.github.com>
2024-05-07 19:03:16 +0530

    Add files via upload

---
+++ src/main/java/com/InternalAssessment/src/test/java/Assessmentquestionone/Pattern2.java
@@ -0,0 +1,19 @@
+package Assessmentquestionone;
+
+public class Pattern2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int rows =4;
+		for (int i = 1; i <= rows; ++i) {
+            for (int j = 1; j <= rows-i; ++j) {
+                System.out.print(" ");
+            	}
+            for(int k = 1; k<=i; ++k) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
