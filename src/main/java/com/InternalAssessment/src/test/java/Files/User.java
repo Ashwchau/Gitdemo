06ad6bac4896cb44c29c1c4c2b2b6872a9e22a59
Ashwchau <162965747+Ashwchau@users.noreply.github.com>
2024-05-07 19:02:07 +0530

    Add files via upload

---
+++ src/main/java/com/InternalAssessment/src/test/java/Files/User.java
@@ -0,0 +1,22 @@
+package Files;
+
+public class User {
+	private String name;
+	private String job;
+	public User(String name,String job) {
+		this.name= name;
+		this.job = job;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+}
